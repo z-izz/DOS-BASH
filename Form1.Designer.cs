e7b63375f0d2745d45b6c1881522f5acc419c29c
Winksplorer <winksplorer@gordae.com>
2023-04-10 22:42:55 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace dosh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(296, 113);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Install DOS#BASH";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(296, 113);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Remove DOS#BASH";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 258);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(295, 21);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(318, 291);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "DOS#BASH v1.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dosh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Install();
+        }
+
+        public void Install()
+        {
+            button1.Text = "Installing DOS#BASH...";
+            Directory.CreateDirectory("C:\\dosh\\");
+            // sudo, ls, la, cat, grep, PATH
+            progressBar1.Value = 17;
+            // SUDO
+            string[] lines =
+            {
+                "@echo Set objShell = CreateObject(\"Shell.Application\") > %temp%\\sudo.tmp.vbs",
+                "@echo args = Right(\"%*\", (Len(\"%*\") - Len(\"%1\"))) >> %temp%\\sudo.tmp.vbs",
+                "@echo objShell.ShellExecute \"%1\", args, \"\", \"runas\" >> %temp%\\sudo.tmp.vbs",
+                "@cscript %temp%\\sudo.tmp.vbs"
+            };
+            File.WriteAllLines("C:\\dosh\\sudo.cmd", lines);
+            progressBar1.Value = 34;
+            // LS
+            File.WriteAllText("C:\\dosh\\ls.cmd", "@echo off\ndir /w");
+            progressBar1.Value = 51;
+            // LA
+            File.WriteAllText("C:\\dosh\\la.cmd", "@echo off\ndir /r");
+            progressBar1.Value = 68;
+            // CAT
+            File.WriteAllText("C:\\dosh\\cat.cmd", "@echo off\ntype %1");
+            progressBar1.Value = 85;
+            // PATH
+            var name = "PATH";
+            var scope = EnvironmentVariableTarget.Machine; // or User
+            var oldValue = Environment.GetEnvironmentVariable(name, scope);
+            var newValue = oldValue + @";C:\dosh\";
+            Environment.SetEnvironmentVariable(name, newValue, scope);
+            progressBar1.Value = 100;
+            button1.Text = "Install DOS#BASH";
+            MessageBox.Show("DOS#BASH successfully installed, restart any CMD windows for changes to take effect.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Remove();
+        }
+
+        public void Remove()
+        {
+            if (!Directory.Exists("C:\\dosh\\"))
+            {
+                MessageBox.Show("There's nothing to remove!");
+
+            }
+            else
+            {
+                button1.Text = "Removing DOS#BASH...";
+                File.Delete("C:\\dosh\\cat.cmd");
+                progressBar1.Value = 17;
+                File.Delete("C:\\dosh\\la.cmd");
+                progressBar1.Value = 34;
+                File.Delete("C:\\dosh\\ls.cmd");
+                progressBar1.Value = 51;
+                File.Delete("C:\\dosh\\sudo.cmd");
+                progressBar1.Value = 68;
+                Directory.Delete("C:\\dosh\\");
+                progressBar1.Value = 85;
+
+                var name = "PATH";
+                var scope = EnvironmentVariableTarget.Machine; // or User
+                var oldValue = Environment.GetEnvironmentVariable(name, scope);
+                var newValue = oldValue.Replace(@";C:\dosh\", "");
+                Environment.SetEnvironmentVariable(name, newValue, scope);
+                progressBar1.Value = 100;
+                button1.Text = "Remove DOS#BASH";
+                MessageBox.Show("DOS#BASH successfully removed, restart any CMD windows for changes to take effect.");
+            }
+        }
+    }
+}
